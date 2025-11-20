dc125c96e6a35ef85b5f8aed2b275d50f31c8628
WonderSTK <mehulparmar9694@gmail.com>
2025-11-20 22:11:23 +0530

    Feat: Extend FormData interface to include education, skills, and themeColor

--- lib/context/FormProvider.tsx
+++ lib/context/FormProvider.tsx
@@ -4,7 +4,51 @@ import { ReactNode, useEffect, useCallback } from "react";
 import { createContext, useState, useContext } from "react";
 import { fetchResume } from "../actions/resume.actions";
 
-const FormContext = createContext({} as any);
+export interface FormData {
+  resumeId?: string;
+  userId?: string;
+  title?: string;
+  updatedAt?: Date;
+  firstName?: string;
+  lastName?: string;
+  jobTitle?: string;
+  address?: string;
+  phone?: string;
+  email?: string;
+  summary?: string;
+  experience: {
+    _id?: string;
+    title?: string;
+    companyName?: string;
+    city?: string;
+    state?: string;
+    startDate?: string;
+    endDate?: string;
+    workSummary?: string;
+  }[];
+  education: {
+    _id?: string;
+    universityName?: string;
+    degree?: string;
+    major?: string;
+    startDate?: string;
+    endDate?: string;
+    description?: string;
+  }[];
+  skills: {
+    _id?: string;
+    name?: string;
+    rating?: number;
+  }[];
+  themeColor?: string;
+}
+
+const FormContext = createContext({} as {
+  formData: FormData;
+  handleInputChange: (e: any) => void;
+  handleRichTextChange: (name: string, value: string) => void;
+  setFormData: React.Dispatch<React.SetStateAction<FormData>>;
+});
 
 const debounce = (func: Function, delay: number) => {
   let timer: NodeJS.Timeout;
@@ -23,7 +67,11 @@ export const FormProvider = ({
   params: { id: string };
   children: ReactNode;
 }) => {
-  const [formData, setFormData] = useState({});
+  const [formData, setFormData] = useState<FormData>({
+    education: [],
+    experience: [],
+    skills: [],
+  });
 
   useEffect(() => {
     const loadResumeData = async () => {
